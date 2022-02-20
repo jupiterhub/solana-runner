025296a3f6a8344089c9683528c428afac0b0a6e
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2022-02-20 22:43:37 +0900

    feat: added rust implementation for counting gif

--- tests/solana-runner.js
+++ tests/solana-runner.js
@@ -1,14 +1,45 @@
-const anchor = require('@project-serum/anchor');
+const anchor = require("@project-serum/anchor");
 
-describe('solana-runner', () => {
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+const main = async () => {
+  console.log("🚀 Starting test...");
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.SolanaRunner;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.solanaRunner;
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
